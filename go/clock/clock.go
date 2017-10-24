77f810124b5b2c035b542779edfac31d69c1734c
wahyu.suadi <wahyu.suadi@gmail.com>
2017-10-24 13:15:51 +0700

    clock belum beres ..

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,18 +1,50 @@
 package clock
 
+import "fmt"
+
 const testVersion = 4
 
 // You can find more details and hints in the test file.
 
-type Clock // Complete the type definition.  Pick a suitable data type.
-
-func New(hour, minute int) Clock {
+// describe clock data structure
+type Clock struct {
+	hour   int
+	minute int
 }
 
-func (Clock) String() string {
+func New(hour, minute int) (result Clock) {
+	result.hour = hour
+	result.minute = minute
+	result.Add(0) // fix hour and minutes
+	return
 }
 
-func (Clock) Add(minutes int) Clock {
+func (c Clock) String() string {
+	hs := fmt.Sprintf("%02d", c.hour)
+	ms := fmt.Sprintf("%02d", c.minute)
+	return hs + ":" + ms
+}
+
+func (c Clock) Add(minutes int) (result Clock) {
+	tmp := c.minute + minutes
+	result = c
+	for {
+		if tmp >= 60 {
+			result.hour++
+			tmp = tmp - 60
+			continue
+		}
+		result.minute = tmp
+		break
+	}
+	for {
+		if result.hour >= 24 {
+			result.hour = result.hour - 24
+			continue
+		}
+		break
+	}
+	return
 }
 
 // Remember to delete all of the stub comments.
