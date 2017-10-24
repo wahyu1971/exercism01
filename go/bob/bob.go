f8ec019a3d73b3b78a585bd36666d3b4ac94227f
wahyu.suadi <wahyu.suadi@gmail.com>
2017-10-25 05:33:52 +0700

    bob selesai, kerja dulu dah...

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,13 +1,28 @@
 package bob
 
-import "regexp"
+import (
+	"regexp"
+	"strings"
+)
 
 const testVersion = 3
 
 func Hey(in string) string {
-	re_sure := regexp.MustCompile("^.*?$")
-	re_whoa := regexp.MustCompile("^[!@#$%^&*A-Z]+$")
-	re_fine := regexp.MustCompile("^[!@#$%^&*A-Z]+$")
+	re_has_upper := regexp.MustCompile("[[:upper:]]")
+	re_sure := regexp.MustCompile("^.*\\?$") // ?
+	re_whoa := regexp.MustCompile("^[[:upper:][:punct:][[:blank:][:digit:]]+$")
+	re_fine := regexp.MustCompile("^[[:space:]]*$")
 
-	return "WHOA!"
+	in = strings.Trim(in, " \t")
+	switch {
+	case re_has_upper.MatchString(in) && re_whoa.MatchString(in):
+		return "Whoa, chill out!"
+	case re_sure.MatchString(in):
+		return "Sure."
+	case re_fine.MatchString(in):
+		return "Fine. Be that way!"
+	default:
+		return "Whatever."
+	}
+
 }
